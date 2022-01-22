cc5e45c5ccf81291afa08b8a3056a56e3701bff7
KimYangOfCat <kim.yang.gh@outlook.com>
2022-01-22 15:19:15 +0800

    change sidebar

--- weeklys/.vuepress/config/sidebar.js
+++ weeklys/.vuepress/config/sidebar.js
@@ -48,28 +48,53 @@ const mobile = {
 	}
 }
 
-const result = tool.formatSideBar([css, js, front, react, mobile, node])
+// const result = tool.formatSideBar([css, js, front, react, mobile, node])
 
-module.exports = result
-// {
-
-// 	[`${css.path}`]: [{
-// 		title: 'CSS Weekly',
-// 		children: [
-// 			{
-// 				title: '#486',
-// 				collapsable: true,
-// 				children: [
-// 					`${css.path}486/toc.md`,
-// 					`${css.path}486/cutsom_properties.md`,
-// 					`${css.path}486/has_selector.md`
-// 				]
-// 			}
-// 		]
-// 	}],
-// 	// '/frontend_focus/': formatSideBar(front),
-// 	// '/javascript_weekly/': formatSideBar(js),
-// 	// '/mobile_dev_weekly': formatSideBar(front)(mobile),
-// 	// '/node_weekly/': formatSideBar(node),
-// 	// '/react_status_weekly/': formatSideBar(react),
-// }
+module.exports = 
+{
+	[`${js.path}`]: [{
+        title: 'JS # 399',   // 必要的
+        path: `${js.path}399/`,      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        sidebarDepth: 1,    // 可选的, 默认值是 1
+        children: [
+			`${js.path}399/js_engine_optimizing_prototype.md`,
+			`${js.path}399/js_engine_shape_and_inline_caches.md`
+        ]
+      },
+	  {
+        title: 'JS #523',   // 必要的
+        path: `${js.path}523/`,      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        sidebarDepth: 1,    // 可选的, 默认值是 1
+        children: [
+			`${js.path}523/undefined_VS_null.md`
+        ]
+      }
+	  ,
+	  {
+        title: 'JS #554',   // 必要的
+        path: `${js.path}554/`,      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 2,    // 可选的, 默认值是 1
+        children: [
+			`${js.path}554/ES2022_Feature_Class_Static_Initialization_Blocks.md`
+        ]
+      }
+	  ,
+	  {
+        title: 'JS #570',   // 必要的
+        path: `${js.path}570/`,      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        collapsable: false,
+		sidebarDepth: 1,    // 可选的, 默认值是 1
+        children: [
+			`${js.path}570/build_tools_compare.md`,
+			`${js.path}570/ES2021_new_feature.md`,
+			`${js.path}570/svelte_vs_react.md`,
+			`${js.path}570/ternary.md`
+        ]
+      }],
+	// '/frontend_focus/': formatSideBar(front),
+	// '/javascript_weekly/': formatSideBar(js),
+	// '/mobile_dev_weekly': formatSideBar(front)(mobile),
+	// '/node_weekly/': formatSideBar(node),
+	// '/react_status_weekly/': formatSideBar(react),
+}
---
+++ weeklys/javascript_weekly/399/README.md
@@ -0,0 +1,9 @@
+---
+title: '#399'
+date: 2022-01-20 02:22:00
+tags:
+ - JavaScript Weekly
+---
+
+1. [JavaScript引擎基础(上)：形态和内联缓存](./javascript_weekly/399/js_engine_shape_and_inline_caches.md) — 对 JavaScript 引擎的工作原理深入了解一下有助于你更好的编写代码。
+2. [JavaScript引擎基础(下)：优化原型](./javascript_weekly/399/js_engine_optimizing_prototype.md) — 对 JavaScript 引擎的工作原理深入了解一下有助于你更好的编写代码。
