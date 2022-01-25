e08ad837875dad1d6ee40b15d61b7e5118689924
KimYangOfCat <kim.yang.gh@outlook.com>
2022-01-26 00:35:41 +0800

    format weekly

--- script/addNewWeekly.js
+++ script/addNewWeekly.js
@@ -6,12 +6,13 @@ const fs = require('fs');
 let req;
 const base = './weeklys'
 const END = `
+
+---
 > * 译文出自：[weekly-tracker](https://github.com/FEDarling/weekly-tracker) 项目，期待你的加入！
 > * [查看原文]()对比阅读
 > * 发现错误？[提交 PR](https://github.com/FEDarling/weekly-tracker/blob/main/)
 > * 译者：
 > * 校对者：
-
 `
 
 const weeklys = [
