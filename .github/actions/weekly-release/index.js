1af660b254d563433ef376e4fc0595f57e2db058
KimYangOfCat <kim.yang.gh@outlook.com>
2022-02-16 17:55:48 +0800

    update Ci

--- .github/actions/weekly-release/index.js
+++ .github/actions/weekly-release/index.js
@@ -2,62 +2,72 @@ const { Toolkit } = require("actions-toolkit");
 const fs = require("fs");
 const yamlFront = require("yaml-front-matter");
 const { Octokit } = require("@octokit/rest");
+const { resolve } = require('path');
 
-const Base = "weeklys/";
-const Weeklys = ['react_status']
+const Base = 'weeklys/';
+const Weeklys = ['react_status'];
 
 // 整理本地待公开周刊
-function getAllUnpublishedAritcles(base,weeklys) {
-  const UnpublishedAritcles = [];
-  weeklys.forEach(weekly => {
-  const weeklyNums = fs.readdirSync(base + weekly);
-    weeklyNums.forEach((weeklyNum) => {
-    // 目前只读取周刊内容
-    let weeklyPath = base + weekly + '/' + weeklyNum + "/README.md"
-    const content = fs.readFileSync(weeklyPath, "utf8");
-    const weeklyNumFront = yamlFront.loadFront(content);
-    if (weeklyNumFront.publish === false) {
-      UnpublishedAritcles.push({
-        title: weeklyNumFront.title,
-        path: weeklyPath,
-      });
-    }
-  });
-  });
-  console.log("隐藏的文章：");
-  console.log(UnpublishedAritcles);
-  return UnpublishedAritcles;
+function getAllUnpublishedAritcles(base, weeklys) {
+    const UnpublishedAritcles = [];
+    weeklys.forEach((weekly) => {
+        const weeklyNums = fs.readdirSync(base + weekly);
+        weeklyNums.forEach((weeklyNum) => {
+            // 目前只读取周刊内容
+            let weeklyPath = base + weekly + '/' + weeklyNum + '/README.md';
+            const content = fs.readFileSync(weeklyPath, 'utf8');
+            const weeklyNumFront = yamlFront.loadFront(content);
+            if (weeklyNumFront.publish === false) {
+                UnpublishedAritcles.push({
+                    title: weeklyNumFront.title,
+                    path: weeklyPath,
+                });
+            }
+        });
+    });
+    console.log('隐藏的文章：');
+    console.log(UnpublishedAritcles);
+    return UnpublishedAritcles;
 }
 
 // 检查未发布的文章
-function getAllUnreleasedAritcles(UnpublishedAritcles) { 
-  const UnreleasedAritcles = [];
-  const octokit = new Octokit({
-      auth: process.env.GITHUB_TOKEN,
-      userAgent: 'Chrome v1.2.3',
-  });
-  let issues = octokit
-      .paginate(
-          'GET /repos/FEDarling/weekly-tracker/issues',
-          { owner: 'FEDarling', repo: 'weekly-tracker' },
-          (response) => response.data.map((issue) => issue.title)
-      )
-      .then((issueTitles, UnreleasedAritcles) => {
-          console.log('已有的issue：');
-          console.log(issueTitles);
-          UnpublishedAritcles.forEach((article) => {
-              if (!issueTitles.includes(article.title)) {
-                  UnreleasedAritcles.push(article);
-              }
-              console.log('无issue的文章1：');
-              console.log(UnreleasedAritcles);
-          });
-      });
-  console.log('无issue的文章：');
-  console.log(UnreleasedAritcles);
-  console.log('issues：');
-  console.log(issues);
-   return UnreleasedAritcles;
+function getAllUnreleasedAritcles(UnpublishedAritcles) {
+    const UnreleasedAritcles = [];
+    const octokit = new Octokit({
+        auth: process.env.GITHUB_TOKEN,
+        userAgent: 'Chrome v1.2.3',
+    });
+    let articles = octokit
+        .paginate(
+            'GET /repos/FEDarling/weekly-tracker/issues',
+            { owner: 'FEDarling', repo: 'weekly-tracker' },
+            (response) => response.data.map((issue) => issue.title)
+        )
+        .then((issueTitles) => {
+            let result = [];
+            console.log('已有的issue：');
+            console.log(issueTitles);
+            UnpublishedAritcles.forEach((article) => {
+                if (!issueTitles.includes(article.title)) {
+                    result.push(article);
+                    console.log(`${article.title} 已添加到待发布列表`);
+                }
+            });
+            console.log('无issue的文章1：');
+            console.log(UnreleasedAritcles);
+            resolve(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    articles.then((result) => {
+        //获取结果
+        UnreleasedAritcles = result;
+        console.log('无issue的文章2：');
+        console.log(UnreleasedAritcles);
+    });
+
+    return UnreleasedAritcles;
 }
 
 function createReleaseIssue() { 
