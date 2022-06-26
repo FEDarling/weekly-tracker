ba1cae4d981a06dc7bc9db58a4354076c294187d
KimYangOfCat <hi@kimyang.cn>
2022-06-26 16:38:27 +0800

    fix rss site url error

--- weeklys/.vuepress/config/plugins.js
+++ weeklys/.vuepress/config/plugins.js
@@ -1,59 +1,74 @@
 const path = require('path')
 module.exports = [
-  [
-    '@vuepress/pwa',
-    {
-      serviceWorker: true,
-      updatePopup: {
-        message: "周刊更新啦！",
-        buttonText: "刷新"
-      }
-    }
-  ],
-  [
-    '@vuepress/plugin-register-components',
-    {
-      components: [
+    [
+        '@vuepress/pwa',
         {
-          name: 'reco-home-page-one',
-          path: path.resolve(__dirname, '../components/HomePageOne.vue')
-        }
-      ],
-      //   componentsDir: path.resolve(__dirname, './demo')
-    }
-  ],
-  ['@vuepress-reco/rss', {
-    site_url: 'https://weekly.fedarling.com/',
-    copyright: 'fedarling 2022',
-    count: 30
-  }],
-  ['@vuepress-reco/bulletin-popover', {
-    body: [
-      {
-        type: 'title',
-        content: '周刊追踪计划启动啦！ 🎉🎉',
-      },
-      {
-        type: 'text',
-        content: `⛱ 目前已有 Mobile Dev Weekly、Frontend Focus、JavaScript Weekly、React Status、Node Weekly 和 CSS Weekly 周刊，欢迎大家阅览，也期待你的加入！🥳`,
-        style: 'font-size: 12px;'
-      },
+            serviceWorker: true,
+            updatePopup: {
+                message: '周刊更新啦！',
+                buttonText: '刷新',
+            },
+        },
     ],
-    footer: [
-      {
-        type: 'button',
-        text: '加入',
-        link: 'https://github.com/FEDarling/weekly-tracker'
-      }
-    ]
-  }],
-  ["vuepress-plugin-nuggets-style-copy", {
-    copyText: "复制代码",
-    tip: {
-      title: '',
-      content: `🎉  复制成功`,
-    },
-  }],
-  // 左侧菜单栏伸缩
-  path.resolve(__dirname, '../plugins/vuepress-plugin-sidebar-collapse/index.js')
-]
+    [
+        '@vuepress/plugin-register-components',
+        {
+            components: [
+                {
+                    name: 'reco-home-page-one',
+                    path: path.resolve(
+                        __dirname,
+                        '../components/HomePageOne.vue'
+                    ),
+                },
+            ],
+            //   componentsDir: path.resolve(__dirname, './demo')
+        },
+    ],
+    [
+        '@vuepress-reco/rss',
+        {
+            site_url: 'https://weekly.fedarling.com',
+            copyright: 'fedarling 2022',
+            count: 30,
+        },
+    ],
+    [
+        '@vuepress-reco/bulletin-popover',
+        {
+            body: [
+                {
+                    type: 'title',
+                    content: '周刊追踪计划启动啦！ 🎉🎉',
+                },
+                {
+                    type: 'text',
+                    content: `⛱ 目前已有 Mobile Dev Weekly、Frontend Focus、JavaScript Weekly、React Status、Node Weekly 和 CSS Weekly 周刊，欢迎大家阅览，也期待你的加入！🥳`,
+                    style: 'font-size: 12px;',
+                },
+            ],
+            footer: [
+                {
+                    type: 'button',
+                    text: '加入',
+                    link: 'https://github.com/FEDarling/weekly-tracker',
+                },
+            ],
+        },
+    ],
+    [
+        'vuepress-plugin-nuggets-style-copy',
+        {
+            copyText: '复制代码',
+            tip: {
+                title: '',
+                content: `🎉  复制成功`,
+            },
+        },
+    ],
+    // 左侧菜单栏伸缩
+    path.resolve(
+        __dirname,
+        '../plugins/vuepress-plugin-sidebar-collapse/index.js'
+    ),
+];
